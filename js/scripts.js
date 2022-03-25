e7bcc92445f596a5e7a10ba0dd13a2de9f689624
BriChavez <brianachavez@gmail.com>
2022-03-25 13:58:09 -0700

    save for restart due to computer problems

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,21 @@ $(document).ready(function() {
     const answer1 = parseInt($("#question1").val());
     const answer2 = parseInt($("#question2").val());
     const answer3 = parseInt($("#question3").val());
-    const total = answer1 + answer2 + answer3;
+    const answer4 = parseInt($("#question3").val());
+    const total = answer1 + answer2 + answer3 + answer4;
 
     console.log(total)
     let result;
     let image;
-    if(total <= 6) {
+    if(total <= 4) {
       result = "C#"
-      image = "<img src='img/c#.jpg'>"
-    } else if(total <= 14) {
+      image = "<img src='images/c#.jpg'>"
+    } else if(total <= 8) {
       result = "Java"
-      image = "<img src='img/java.jpg'>"
-    } else if(total <= 17) {
+      image = "<img src='images/java.jpg'>"
+    } else if(total <= 12) {
       result = "Ruby"
-      image = "<img src='img/ruby.jpg'>"
+      image = "<img src='images/ruby.jpg'>"
     }
     $("#output").text("");
     $("#output").append("<p>" + result + "</p>");
