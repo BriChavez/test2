3cdb34c740d527a44c04faf5ea65252344f3d599
BriChavez <brianachavez@gmail.com>
2022-03-25 16:51:16 -0700

    adds a read me

---
+++ README.md
@@ -0,0 +1,38 @@
+# _Week 2 test_
+
+#### By _**BriChavez**_
+
+#### _{Brief description of application}_
+
+## Technologies Used
+
+* Java
+* jQuery
+* HTML
+* Bootstrap
+* CSS
+
+## Description
+
+This website allows the user to take a brief quiz to help determine what type of scripting they should go into.
+
+## Setup/Installation Requirements
+
+* Go to gitHub
+* Click on the my second test repository
+* Copy the url
+* Using the terminal, clone the site
+* Run the program
+
+
+## Known Bugs
+
+* Image doesnt properly disappear when you click submit
+* Background doesnt change when you click submit
+* Syntax errors
+
+## License
+
+If you can help me figure out how to properly do what i want the page to do, i would love any advice you can offer me. Please email me at brianachavez@gmail.com
+
+Copyright (c) _03/25/2022_ _BriChavez(s)_
--- js/scripts.js
+++ js/scripts.js
@@ -13,24 +13,31 @@ $(document).ready(function() {
     if(total <= 4) {
       result = "C# is a general-purpose, multi-paradigm programming language. C# encompasses static typing, strong typing, lexically scoped, imperative, declarative, functional, generic, object-oriented (class-based), and component-oriented programming disciplines."
       image = "<img src='images/csharp.jpg'>"
+      $("h2").hide();
       $("body").removeClass();
-      $("body").addClass("cSharp-background");
+      $("script").hide();
+      $('.cSharp-background').show();
     } else if(total <= 8) {
       result = "JavaScript is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. Over 97% of websites use JavaScript on the client side for web page behavior, often incorporating third-party libraries."
       image = "<img src='images/java.jpg'>"
+      $("h2").hide();
+      $("script").hide();
       $("body").removeClass();
-      $("body").addClass("java-background");
+      $('.java-background').show();
+      
     } else if(total <= 12) {
       result = "Ruby is an interpreted, high-level, general-purpose programming language which supports multiple programming paradigms. It was designed with an emphasis on programming productivity and simplicity. In Ruby, everything is an object, including primitive data types."
       image = "<img src='images/ruby.jpg'>"
+      $("h2").hide();
+      $("script").hide();
       $("body").removeClass();
-      $("body").addClass("ruby-background");
+      $('.ruby-background').show();
     }
     $("#output").text("");
     $("#output").append("<p>" + result + "</p>");
     $("#output").append(image);
     $("button#submit").click(function() {
-    $("body").hide();
+ 
 
       
     });
