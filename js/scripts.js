b2b71e1d04fcc7f1fe07f45a0fc2b793f9e3df21
BriChavez <brianachavez@gmail.com>
2022-03-25 16:15:19 -0700

    change background colors when submit is pressed

--- js/scripts.js
+++ js/scripts.js
@@ -13,19 +13,26 @@ $(document).ready(function() {
     if(total <= 4) {
       result = "C# is a general-purpose, multi-paradigm programming language. C# encompasses static typing, strong typing, lexically scoped, imperative, declarative, functional, generic, object-oriented (class-based), and component-oriented programming disciplines."
       image = "<img src='images/csharp.jpg'>"
+      $("body").removeClass();
+      $("body").addClass("cSharp-background");
     } else if(total <= 8) {
       result = "JavaScript is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. Over 97% of websites use JavaScript on the client side for web page behavior, often incorporating third-party libraries."
       image = "<img src='images/java.jpg'>"
+      $("body").removeClass();
+      $("body").addClass("java-background");
     } else if(total <= 12) {
       result = "Ruby is an interpreted, high-level, general-purpose programming language which supports multiple programming paradigms. It was designed with an emphasis on programming productivity and simplicity. In Ruby, everything is an object, including primitive data types."
       image = "<img src='images/ruby.jpg'>"
+      $("body").removeClass();
+      $("body").addClass("ruby-background");
     }
     $("#output").text("");
     $("#output").append("<p>" + result + "</p>");
     $("#output").append(image);
-    $("button#btn").click(function() {
-      $("button#btn").removeClass();
-      $("body").addClass("java-background");
+    $("button#submit").click(function() {
+    $("body").hide();
+
+      
     });
   });
 });
