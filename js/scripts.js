2f6640d2a1618737dbbe8b87181739154e428d3b
BriChavez <brianachavez@gmail.com>
2022-03-25 17:21:57 -0700

    adds a 5th question and removes some empty lines of code

--- js/scripts.js
+++ js/scripts.js
@@ -5,27 +5,26 @@ $(document).ready(function() {
     const answer2 = parseInt($("#question2").val());
     const answer3 = parseInt($("#question3").val());
     const answer4 = parseInt($("#question3").val());
-    const total = answer1 + answer2 + answer3 + answer4;
+    const answer5 = parseInt($("#question3").val());
+    const total = answer1 + answer2 + answer3 + answer4 + answer5;
 
-    console.log(total)
     let result;
     let image;
-    if(total <= 4) {
+    if(total <= 5) {
       result = "C# is a general-purpose, multi-paradigm programming language. C# encompasses static typing, strong typing, lexically scoped, imperative, declarative, functional, generic, object-oriented (class-based), and component-oriented programming disciplines."
       image = "<img src='images/csharp.jpg'>"
       $("h2").hide();
       $("body").removeClass();
       $("script").hide();
       $('.cSharp-background').show();
-    } else if(total <= 8) {
+    } else if(total <= 10) {
       result = "JavaScript is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. Over 97% of websites use JavaScript on the client side for web page behavior, often incorporating third-party libraries."
       image = "<img src='images/java.jpg'>"
       $("h2").hide();
       $("script").hide();
       $("body").removeClass();
       $('.java-background').show();
-      
-    } else if(total <= 12) {
+    } else if(total <= 15) {
       result = "Ruby is an interpreted, high-level, general-purpose programming language which supports multiple programming paradigms. It was designed with an emphasis on programming productivity and simplicity. In Ruby, everything is an object, including primitive data types."
       image = "<img src='images/ruby.jpg'>"
       $("h2").hide();
@@ -37,9 +36,6 @@ $(document).ready(function() {
     $("#output").append("<p>" + result + "</p>");
     $("#output").append(image);
     $("button#submit").click(function() {
- 
-
-      
     });
   });
 });
